3e03fc1daee89c60edb1e7c9632aeb86e9d8228e
Zhongqiang Richard Ren <ren.zhongqiang@outlook.com>
2022-04-14 10:45:12 -0400

    Qf must be very large so that the code works on the toy example

--- test_dirCol.py
+++ test_dirCol.py
@@ -17,20 +17,128 @@ def main1():
   
   np.set_printoptions(precision=3, suppress=True)
 
-  N = 11
   xo = np.array([1, 3, 0, 0, 0])
-  Uinit = np.array([[1,1],[1,0],[1,0],[1,0],[1,0],[-1,0],[-1,-1],[-1,0],[-1,0],[-1,0]])
-  Xinit = np.zeros([11,5])
+
+  N = 11
+  Uinit = np.array([[1,1],[1,1],[1,1],[1,1],[1,1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]])
+
+  Xinit = np.zeros([N,5])
   Xinit[0,:] = xo
-  dt = 0.1
+  dt = 0.2
   for k in range(len(Uinit)):
     u = Uinit[k]
     Xinit[k+1,:] = dyn.fd_ddc2(Xinit[k,:],u,dt)
   xf = Xinit[N-1,:]
+  xf[2:5] = 0
+
+  Ulb = np.array([-1.5,-1.5])
+  Uub = np.array([ 1.5, 1.5])
+
+  # obs_pos_array = np.array([[1,2],[2,2],[3,2],[3,4],[4,4],[5,4]])
+  obs_pos_array = np.array([[-1,-1]])
+  obss = obs.ObstSet( obs_pos_array )
+
+  nlp_prob = dc.NLP_ddc2(xo, xf, N, dt, Ulb, Uub, Xinit, Uinit, obss, w=100.0)
+
+  Zinit = nlp_prob.toZ(nlp_prob.Xinit, nlp_prob.Uinit)
+
+  zlb = nlp_prob.Zlb()
+  print("zlb = ", zlb)
+  zub = nlp_prob.Zub()
+  print("zub = ", zub)
+
+  nlp = cyipopt.Problem(
+     n=nlp_prob.lenZ,
+     m=nlp_prob.lenC,
+     problem_obj=nlp_prob,
+     lb=zlb,
+     ub=zub,
+     cl=np.ones(nlp_prob.lenC)*(-0.01),
+     cu=np.ones(nlp_prob.lenC)*(0.01),
+  )
+
+  nlp.add_option('mu_strategy', 'adaptive')
+  # nlp.add_option('tol', 1e-3)
+  nlp.add_option('max_iter', 1000)
+
+  Zsol, info = nlp.solve(Zinit)
+
+  print("================================================")
+  print(Zsol)
+  print(info)
+
+  print(" constr eval = ", nlp_prob.constraints(Zsol) )
+  print(" obj eval = ", nlp_prob.objective(Zsol) )
+
+  # print("Z = ", Zsol)
+  # print("grad = ", nlp_prob.gradient(Zsol))
+
+
+  # fig = plt.figure(figsize=(3,3))
+  fig = plt.figure()
+
+  xy_tj = nlp_prob.getTrajXY(Zinit)
+  plt.plot(xy_tj[:,0],xy_tj[:,1],"ro--")
+
+  # print("***********")
+  # print("diff = ", Zsol-Zinit)
+  # print("state 0 = ",  nlp_prob.Zx(Zsol,0) ) 
+  # print("state 1 = ",  nlp_prob.Zx(Zsol,1) ) 
+
+  xy_tj = nlp_prob.getTrajXY(Zsol)
+  plt.plot(xy_tj[:,0],xy_tj[:,1],"bo--")
+
+  plt.show()
+
+
+  return
+
+def main2():
+  
+  # xo = np.array([3, 1, 0, 0, 0])
+  # xf = np.array([3, 5, -np.pi, 0, 0])
+  
+  np.set_printoptions(precision=3, suppress=True)
+
+  # xo = np.array([1, 3, 0, 0, 0])
+
+  # N = 4
+  # Uinit = np.array([[1,1],[0,0],[-1,-1]])
+
+  # N = 11
+  # Uinit = np.array([[1,1],[1,1],[1,1],[1,1],[1,1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]])
+
+  # Xinit = np.zeros([N,5])
+  # Xinit[0,:] = xo
+  # dt = 0.1
+  # for k in range(len(Uinit)):
+  #   u = Uinit[k]
+  #   Xinit[k+1,:] = dyn.fd_ddc2(Xinit[k,:],u,dt)
+  # xf = Xinit[N-1,:]
+  # xf[2:5] = 0
+
+  N = 11
+  dt = 0.1
+  Xinit = np.array([[1,3,0,0,0],
+                    [1,3,0,0,0],
+                    [2,3,0,0,0],
+                    [3,3,0,0,0],
+                    [4,3,0,0,0],
+                    [5,3,0,0,0],
+                    [5,4,0,0,0],
+                    [6,5,0,0,0],
+                    [6,6,0,0,0],
+                    [6,7,0,0,0],
+                    [6,8,np.pi/2,0,0]])
+  xo = Xinit[0,:]
+  xf = Xinit[N-1,:]
+  Uinit = np.zeros([N-1,2])
+
+  print("================================================")
   print("Xinit = ",Xinit)
 
-  Ulb = np.array([-2,-2])
-  Uub = np.array([ 2, 2])
+  Ulb = np.array([-1.5,-1.5])
+  Uub = np.array([ 1.5, 1.5])
   
   # Xinit = np.zeros([N,5])
   # for k in range(5):
@@ -47,33 +155,50 @@ def main1():
 
   Zinit = nlp_prob.toZ(nlp_prob.Xinit, nlp_prob.Uinit)
 
-  print(" constr eval = ", nlp_prob.constraints(Zinit) )
-  print(" obj eval = ", nlp_prob.objective(Zinit) )
+  # print(" constr eval = ", nlp_prob.constraints(Zinit) )
+  # print(" obj eval = ", nlp_prob.objective(Zinit) )
 
-  print("Zinit = ", Zinit)
-  print("grad = ", nlp_prob.gradient(Zinit))
-  jac = nlp_prob.jacobian(Zinit)
-  print("jac = ", jac.shape)
+  # print("Zinit = ", Zinit)
+  # print("grad = ", nlp_prob.gradient(Zinit))
+  # jac = nlp_prob.jacobian(Zinit)
+  # print("jac = ", jac.shape)
+
+  # for k in range(N):
+  #   print("Zx[k] = ", nlp_prob.Zx(Zinit,k))
+  #   print("Zu[k] = ", nlp_prob.Zu(Zinit,k))
+
+  zlb = nlp_prob.Zlb()
+  print("zlb = ", zlb)
+  zub = nlp_prob.Zub()
+  print("zub = ", zub)
 
   nlp = cyipopt.Problem(
      n=nlp_prob.lenZ,
      m=nlp_prob.lenC,
      problem_obj=nlp_prob,
-     lb=nlp_prob.Zlb(),
-     ub=nlp_prob.Zub(),
-     cl=np.ones(nlp_prob.lenC)*(-0.5),
-     cu=np.ones(nlp_prob.lenC)*(0.5),
+     lb=zlb,
+     ub=zub,
+     cl=np.ones(nlp_prob.lenC)*(-0.01),
+     cu=np.ones(nlp_prob.lenC)*(0.01),
   )
 
 
   # nlp.add_option('mu_strategy', 'adaptive')
   # nlp.add_option('tol', 1e-3)
-  nlp.add_option('max_iter', 10)
+  nlp.add_option('max_iter', 1000)
 
-  x, info = nlp.solve(nlp_prob.toZ(nlp_prob.Xinit, nlp_prob.Uinit))
+  Zsol, info = nlp.solve(Zinit)
 
-  print(x)
+  print("================================================")
+  print(Zsol)
   print(info)
+
+  print(" constr eval = ", nlp_prob.constraints(Zsol) )
+  print(" obj eval = ", nlp_prob.objective(Zsol) )
+
+  # print("Z = ", Zsol)
+  # print("grad = ", nlp_prob.gradient(Zsol))
+
 
   # fig = plt.figure(figsize=(3,3))
   fig = plt.figure()
@@ -81,7 +206,12 @@ def main1():
   xy_tj = nlp_prob.getTrajXY(Zinit)
   plt.plot(xy_tj[:,0],xy_tj[:,1],"ro--")
 
-  xy_tj = nlp_prob.getTrajXY(x)
+  # print("***********")
+  # print("diff = ", Zsol-Zinit)
+  # print("state 0 = ",  nlp_prob.Zx(Zsol,0) ) 
+  # print("state 1 = ",  nlp_prob.Zx(Zsol,1) ) 
+
+  xy_tj = nlp_prob.getTrajXY(Zsol)
   plt.plot(xy_tj[:,0],xy_tj[:,1],"bo--")
 
   plt.show()
--- tools/dirCol.py
+++ tools/dirCol.py
@@ -28,16 +28,23 @@ class NLP_ddc2:
     self.Uub = Uub
 
     self.w = w
-    self.R = np.identity(2)*0.05
-    self.Qf = np.identity(5)
+    self.R = np.identity(2)*0.1
+    self.Q = np.identity(5)*0.1
+    self.Q[2:5,2:5] = np.zeros([3,3])
+    self.Qf = np.identity(5)*1000
 
     # self.lenZ = self.n+(self.N-1)*(self.m+self.n) # num of decision variables, [x0,u0,x1,u1,x2,...,nN-2,xN-1]
     self.lenZ = (self.N)*(self.m+self.n) # num of decision variables, [x0,u0,x1,u1,x2,...,nN-2,xN-1,uN-1]
     self.lenC = self.n + (self.N-1)*self.n + self.n # num of constraints, x0 is also added as constraints here.
 
+    self.fXk = list() # of length N
     self.Xm = np.zeros([self.N-1,self.n])
     self.Um = np.zeros([self.N-1,self.m])
     self.fXm = np.zeros([self.N-1,self.n])
+    self.Ak = list()
+    self.Bk = list()
+    self.Am = list()
+    self.Bm = list()
 
     return
 
@@ -116,24 +123,35 @@ class NLP_ddc2:
     """
     return the scalar objective value given decision variable Z.
     """
+    m = self.m
+    n = self.n
 
     # obst cost
     self.xy_tj = self.getTrajXY(Z) # cache the result for gradient usage.
     J1 = self.w*self.obss.arrayCost(self.xy_tj[1:])
 
+    # state cost
+    J2 = 0
+    for k in range(self.N-1):
+      a = (k)*(n+m)
+      # grad[a+n:a+n+2] += dJ2dU[k]
+      dx = self.Zx(Z,k) - self.Xinit[k]
+      dx[2:5] = 0
+      J2 += 0.5*np.dot( np.dot(self.Q, dx), dx )
+
     # control cost
     self.u_tj = self.getTrajU(Z) # cache the result for gradient usage.
     self.u_tj_R = np.dot(self.u_tj,self.R) # cache the result for gradient usage.
-    J2 = 0.5*np.sum(self.u_tj_R*self.u_tj)
+    J3 = 0.5*np.sum(self.u_tj_R*self.u_tj)
 
     # final state cost
     dxf = self.Zx(Z,self.N-1) - self.xf
     dx_Qf = np.dot(dxf,self.Qf)
-    J3 = 0.5*np.dot(dx_Qf, dxf)
+    J4 = 0.5*np.dot(dx_Qf, dxf)
 
     # print("exit objective")
     # return J1+J2+J3
-    return J2+J3
+    return J1+J2+J3+J4
 
   def gradient(self, Z):
     """
@@ -142,30 +160,82 @@ class NLP_ddc2:
     grad = np.zeros(self.lenZ)
     m = self.m
     n = self.n
+    In = np.identity(n)
+    Im = np.identity(m)
 
-    # # obst cost
-    # self.xy_tj = self.getTrajXY(Z) # cache the result for gradient usage.
-    # dJ1dXY = self.obss.arrayGrad(self.xy_tj) # include x0
-    # # print("dJ1dXY shape = ", dJ1dXY.shape)
-    # for k in range(self.N-1):
-    #   a = (k)*(n+m)
-    #   grad[a:a+2] += dJ1dXY[k]
+    # evaluate dynamics at x0,x1,,.,xN-1 (totally N points)
+    self.fXk = list() # of length N
+    for k in range(self.N):
+      x = self.Zx(Z,k)
+      u = self.Zu(Z,k)
+      self.fXk.append( dyn.fc_ddc2(x,u) )
     
-    # control cost
-    self.u_tj = self.getTrajU(Z) # cache the result for gradient usage.
-    dJ2dU = np.dot(self.R,self.u_tj.T).T
-    # print("dJ2dU shape = ", dJ2dU.shape)
+    # evalute collocation points
+    self.Xm = np.zeros([self.N-1,n])
+    self.Um = np.zeros([self.N-1,m])
+    self.fXm = np.zeros([self.N-1,n])
+    for k in range(self.N-1):
+      self.Um[k] = 0.5*( self.Zu(Z,k) + self.Zu(Z,k+1) )
+      self.Xm[k] = 0.5*( self.Zx(Z,k) + self.Zx(Z,k+1) ) + self.dt/8.0*(self.fXk[k] - self.fXk[k+1])
+      self.fXm[k] = dyn.fc_ddc2(self.Xm[k], self.Um[k]) # at time t+self.dt/2.0
+    
+    # evaluate linearized dynamics
+    self.Ak = list()
+    self.Bk = list()
+    for k in range(self.N):
+      u = self.Zu(Z,k)
+      self.Ak.append( dyn.fc_ddc2_A(self.Zx(Z,k), u) )
+      self.Bk.append( dyn.fc_ddc2_B(self.Zx(Z,k), u) )
+
+    # evaluate linearized dynamics
+    self.Am = list()
+    self.Bm = list()
+    for k in range(self.N-1):
+      self.Am.append( dyn.fc_ddc2_A(self.Xm[k],self.Um[k]) )
+      self.Bm.append( dyn.fc_ddc2_B(self.Xm[k],self.Um[k]) )
+
+    # 1 obst cost grad
+    self.xy_tj = self.getTrajXY(Z) # cache the result for gradient usage.
+    dJ1dXY = self.obss.arrayGrad(self.xy_tj) # include x0
+    # print("dJ1dXY shape = ", dJ1dXY.shape)
     for k in range(self.N-1):
       a = (k)*(n+m)
-      # grad[a+n:a+n+2] += dJ2dU[k]
-      grad[a+n:a+n+2] += np.dot(self.R,self.u_tj[k])
+      grad[a:a+2] += dJ1dXY[k]
 
-    # final state cost
+    # 2 stage state cost and 3 control cost
+    for k in range(self.N-1):
+      a = (k)*(n+m)
+      x1 = self.Zx(Z,k)
+      x2 = self.Zx(Z,k+1)
+      u1 = self.Zu(Z,k)
+      u2 = self.Zu(Z,k+1)
+      dx = x1 - self.Xinit[k]
+      dl_dx1 = np.dot(self.Q,dx)
+      dl_dxm = np.dot(self.Q,self.Xm[k])
+      dxm_dx1 = 0.5*In + self.dt/8 * self.Ak[k]
+      grad[a:a+n] += self.dt/6* (dl_dx1 + 4*np.dot(dxm_dx1.T,dl_dxm) ) # ?
+
+      dl_du1 = np.dot(self.R, u1)
+      dl_dum = np.dot(self.R, self.Um[k])
+      dum_du1 = 0.5*Im
+      dxm_du1 = self.dt/8*self.Bk[k]
+      grad[a+n:a+n+2] += self.dt/6*(dl_du1 + 4*np.dot(dum_du1, dl_dum) + 4*np.dot(dl_dxm,dxm_du1))   
+
+    # # 3 control cost
+    # self.u_tj = self.getTrajU(Z) # cache the result for gradient usage.
+    # dJ2dU = np.dot(self.R,self.u_tj.T).T
+    # # print("dJ2dU shape = ", dJ2dU.shape)
+    # for k in range(self.N-1):
+    #   a = (k)*(n+m)
+    #   # grad[a+n:a+n+2] += dJ2dU[k]
+    #   grad[a+n:a+n+2] += np.dot(self.R,self.u_tj[k])
+
+    # 4 final state cost
     dxf = self.Zx(Z,self.N-1) - self.xf
     dJ3dxf = np.dot(self.Qf, dxf)
     a = (self.N-1)*(n+m)
     grad[a:a+n] += dJ3dxf
-
+    # print(">>> >>> grad = ", grad)
     return grad
 
   # def hessian(self, x, lagrange, obj_factor):
@@ -186,7 +256,6 @@ class NLP_ddc2:
 
     # evaluate dynamics at x0,x1,,.,xN-1 (totally N points)
     self.fXk = list() # of length N
-    # print("constraints")
     for k in range(self.N):
       x = self.Zx(Z,k)
       u = self.Zu(Z,k)
@@ -196,23 +265,40 @@ class NLP_ddc2:
     self.Xm = np.zeros([self.N-1,n])
     self.Um = np.zeros([self.N-1,m])
     self.fXm = np.zeros([self.N-1,n])
-    # print("constraints2")
     for k in range(self.N-1):
       self.Um[k] = 0.5*( self.Zu(Z,k) + self.Zu(Z,k+1) )
       self.Xm[k] = 0.5*( self.Zx(Z,k) + self.Zx(Z,k+1) ) + self.dt/8.0*(self.fXk[k] - self.fXk[k+1])
       self.fXm[k] = dyn.fc_ddc2(self.Xm[k], self.Um[k]) # at time t+self.dt/2.0
+    
+    # evaluate linearized dynamics
+    self.Ak = list()
+    self.Bk = list()
+    for k in range(self.N):
+      u = self.Zu(Z,k)
+      self.Ak.append( dyn.fc_ddc2_A(self.Zx(Z,k), u) )
+      self.Bk.append( dyn.fc_ddc2_B(self.Zx(Z,k), u) )
+
+    # evaluate linearized dynamics
+    self.Am = list()
+    self.Bm = list()
+    for k in range(self.N-1):
+      self.Am.append( dyn.fc_ddc2_A(self.Xm[k],self.Um[k]) )
+      self.Bm.append( dyn.fc_ddc2_B(self.Xm[k],self.Um[k]) )
 
     # initial state
     C[0:n] = self.Zx(Z,0) - self.xo
+    # print(" xo = ", self.xo, " self.Zx(Z,0) = ", self.Zx(Z,0), " self.Zx(Z,1) = ", self.Zx(Z,1))
 
     # dynamics constraints
     for k in range(self.N-1):
       a = n + k*n
       C[a:a+n] = ( self.Zx(Z,k) - self.Zx(Z,k+1) ) + self.dt/6.0 * (self.fXk[k] + 4*self.fXm[k] + self.fXk[k+1])
+      C[a+2:a+5] = 0
 
     # final state
     C[n+(self.N-1)*n : n+self.N*self.n] = self.Zx(Z,self.N-1) - self.xf
 
+    # print(">>> C[0] = ", C[0])
     return C
 
   def jacobian(self, Z):
@@ -226,22 +312,36 @@ class NLP_ddc2:
     m = self.m
     n = self.n
 
-    # print("jacobian")
-
-    Ak = list()
-    Bk = list()
+    # evaluate dynamics at x0,x1,,.,xN-1 (totally N points)
+    self.fXk = list() # of length N
+    for k in range(self.N):
+      x = self.Zx(Z,k)
+      u = self.Zu(Z,k)
+      self.fXk.append( dyn.fc_ddc2(x,u) )
+    
+    # evalute collocation points
+    self.Xm = np.zeros([self.N-1,n])
+    self.Um = np.zeros([self.N-1,m])
+    self.fXm = np.zeros([self.N-1,n])
+    for k in range(self.N-1):
+      self.Um[k] = 0.5*( self.Zu(Z,k) + self.Zu(Z,k+1) )
+      self.Xm[k] = 0.5*( self.Zx(Z,k) + self.Zx(Z,k+1) ) + self.dt/8.0*(self.fXk[k] - self.fXk[k+1])
+      self.fXm[k] = dyn.fc_ddc2(self.Xm[k], self.Um[k]) # at time t+self.dt/2.0
+    
+    # evaluate linearized dynamics
+    self.Ak = list()
+    self.Bk = list()
     for k in range(self.N):
       u = self.Zu(Z,k)
-      Ak.append( dyn.fc_ddc2_A(self.Zx(Z,k), u) )
-      Bk.append( dyn.fc_ddc2_B(self.Zx(Z,k), u) )
+      self.Ak.append( dyn.fc_ddc2_A(self.Zx(Z,k), u) )
+      self.Bk.append( dyn.fc_ddc2_B(self.Zx(Z,k), u) )
 
-    # print("jacobian2")
-
-    Am = list()
-    Bm = list()
+    # evaluate linearized dynamics
+    self.Am = list()
+    self.Bm = list()
     for k in range(self.N-1):
-      Am.append( dyn.fc_ddc2_A(self.Xm[k],self.Um[k]) )
-      Bm.append( dyn.fc_ddc2_B(self.Xm[k],self.Um[k]) )
+      self.Am.append( dyn.fc_ddc2_A(self.Xm[k],self.Um[k]) )
+      self.Bm.append( dyn.fc_ddc2_B(self.Xm[k],self.Um[k]) )
 
     In = np.identity(n)
     Im = np.identity(m)
@@ -254,21 +354,21 @@ class NLP_ddc2:
       col_idx = k*(m+n)
       # print(" prev, k = ", k, " col_idx = ", col_idx)
       
-      dxm_dx1 = 0.5*In + self.dt/8.0 * Ak[k]
-      jac[row_idx:row_idx+n, col_idx:col_idx+n] += In + self.dt/6*(Ak[k] + 4*np.dot(Am[k],dxm_dx1) )
+      dxm_dx1 = 0.5*In + self.dt/8.0 * self.Ak[k]
+      jac[row_idx:row_idx+n, col_idx:col_idx+n] += In + self.dt/6*(self.Ak[k] + 4*np.dot(self.Am[k],dxm_dx1) )
 
-      dxm_dx2 = 0.5*In - self.dt/8.0 * Ak[k]
-      jac[row_idx:row_idx+n, col_idx+m+n:col_idx+m+n+n] += (-In) + self.dt/6*(Ak[k+1] + 4*np.dot(Am[k],dxm_dx2) )
+      dxm_dx2 = 0.5*In - self.dt/8.0 * self.Ak[k]
+      jac[row_idx:row_idx+n, col_idx+m+n:col_idx+m+n+n] += (-In) + self.dt/6*(self.Ak[k+1] + 4*np.dot(self.Am[k],dxm_dx2) )
 
       dum_du1 = 0.5*Im
-      dxm_du1 = self.dt/8.0 * Bk[k]
-      jac[row_idx:row_idx+n, col_idx+n:col_idx+n+m] += self.dt/6*(Bk[k] + 4*np.dot(Am[k],dxm_du1) + 4*np.dot(Bm[k],dum_du1) )
+      dxm_du1 = self.dt/8.0 * self.Bk[k]
+      jac[row_idx:row_idx+n, col_idx+n:col_idx+n+m] += self.dt/6*(self.Bk[k] + 4*np.dot(self.Am[k],dxm_du1) + 4*np.dot(self.Bm[k],dum_du1) )
 
       # if k < self.N-2:
       dum_du2 = 0.5*Im
-      dxm_du2 = -self.dt/8.0 * Bk[k+1]
+      dxm_du2 = -self.dt/8.0 * self.Bk[k+1]
       cidx = col_idx+m+n
-      jac[row_idx:row_idx+n, col_idx+n+m+n:col_idx+n+m+n+m] += self.dt/6*(Bk[k+1] + 4*np.dot(Am[k],dxm_du2) + 4*np.dot(Bm[k],dum_du2) )
+      jac[row_idx:row_idx+n, col_idx+n+m+n:col_idx+n+m+n+m] += self.dt/6*(self.Bk[k+1] + 4*np.dot(self.Am[k],dxm_du2) + 4*np.dot(self.Bm[k],dum_du2) )
 
     # final state
     row_idx = n+(self.N-1)*n
@@ -276,26 +376,35 @@ class NLP_ddc2:
     jac[row_idx:row_idx+n, col_idx:col_idx+n] += In
 
     # print("jacobian3")
+    # print(">>> Jac = ", jac)
     return jac
 
 
   def Zlb(self):
     """
     """
-    lb = np.ones(self.lenZ)*(-1e9)
+    lb = np.ones(self.lenZ)*(-10)
     for k in range(self.N-1):
       a = (k)*(self.n+self.m)
-      lb[a : a+self.m] = self.Ulb
+      lb[a+self.n : a+self.m+self.n] = self.Ulb # accel
+      lb[a+3] = -0.5 # min velocity
+      lb[a+4] = -0.5 # min angular vel
+    a = (self.N-1)*(self.n+self.m)
+    lb[a+self.n : a+self.m+self.n] = np.zeros(2)
     return lb
 
 
   def Zub(self):
     """
     """
-    ub = np.ones(self.lenZ)*(1e9)
+    ub = np.ones(self.lenZ)*(10)
     for k in range(self.N-1):
       a = (k)*(self.n+self.m)
-      ub[a : a+self.m] = self.Uub
+      ub[a+self.n : a+self.m+self.n] = self.Uub
+      ub[a+3] = 2.0 # max velocity
+      ub[a+4] = 0.5 # max angular vel
+    a = (self.N-1)*(self.n+self.m)
+    ub[a+self.n : a+self.m+self.n] = np.zeros(2)
     return ub
 
   def intermediate(self, alg_mod, iter_count, obj_value, inf_pr, inf_du, mu,
@@ -307,3 +416,5 @@ class NLP_ddc2:
 
     print(msg.format(iter_count, obj_value))
 
+    # print("---iter(",iter_count,")---")
+    # print(" grad = ", gradient(self.Z))
